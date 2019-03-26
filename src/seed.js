5f4f03b667ec072971e7d1d9182bb6f2f8b249f8
Destry Strauss <destry@mobilelogix.com>
2019-03-26 11:43:52 -0700

    Started adding database seeding

---
+++ src/seed.js
@@ -0,0 +1,4 @@
+// @todo:
+//      - build CLI script that prompts user for initial client and user info
+//      - create structures, if not exists (db_structure.sql)
+//      - create rows, if not exists
